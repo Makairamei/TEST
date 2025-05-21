ad1bd78e810367873d591ddd87e21409d277564b
Makaira <128672138+Makairamei@users.noreply.github.com>
2025-05-21 10:49:24 +0700

    Update Log.js

--- backend/models/Log.js
+++ backend/models/Log.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
+
+const LogSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  username: String,
+  ip: String,
+  device: String,
+  time: { type: Date, default: Date.now },
+  action: String
+});
+
+module.exports = mongoose.model('Log', LogSchema);
 
