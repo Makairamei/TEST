7f959800a0dff418105fd4da7ee17a1bd3c70bd8
Makaira <128672138+Makairamei@users.noreply.github.com>
2025-05-21 12:29:44 +0700

    Update App.js

--- backend/models/App.js
+++ backend/models/App.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
-const AppSchema = new mongoose.Schema({
+const appSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  cookie: { type: String, required: true },
+  description: String,
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('App', AppSchema);
-
+module.exports = mongoose.model('App', appSchema);
