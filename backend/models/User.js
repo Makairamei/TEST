b2645d5cf62a3a4b8274c6b0023a8227da932f82
Makaira <128672138+Makairamei@users.noreply.github.com>
2025-05-21 12:29:33 +0700

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,9 @@ const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   role: { type: String, default: 'user' },
+  lastLogin: { type: Date },
+  activeDevices: { type: Number, default: 0 },
+  loginLocation: { type: String }
 });
 
 module.exports = mongoose.model('User', userSchema);
